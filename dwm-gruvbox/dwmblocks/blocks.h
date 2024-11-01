05705d6fc5f6136f8521e1516f235b4727b76a55
today <135026855+quthery@users.noreply.github.com>
2024-11-01 13:45:00 +0300

    Add files via upload

---
+++ dwm-gruvbox/dwmblocks/blocks.h
@@ -0,0 +1,16 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"┇  ", "xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print \"[RU]\"; else print \"[EN]\"; }'", 0, 1},
+
+  {"⛅ " , "curl -s 'wttr.in/Kursk?format=%t'", 300, 0},
+
+	{"📅 " , "date '+%d.%m'", 60, 0},
+	{"🕒 ", "date '+%H:%M  '", 5, 0},
+	{"🛢️ ", "acpi | awk '{print $4}' | sed s/,//"}
+
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = "  ┇  ";
+static unsigned int delimLen = 7;
